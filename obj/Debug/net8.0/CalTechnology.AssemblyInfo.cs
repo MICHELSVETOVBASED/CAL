6c0a90d98fb4d1be86f84410a10d3c68fcd088ca
maksk <wgagaqwt1@gmail.com>
2025-05-28 22:38:49 +0200

    no comments

--- obj/Debug/net8.0/CalTechnology.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalTechnology.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalTechnology")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07bdb49f3c6f3a2ffa546771bc422dfb81fd22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5b1fedef5dcf550c629e0e35457fb5e2ed32fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalTechnology")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalTechnology")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
